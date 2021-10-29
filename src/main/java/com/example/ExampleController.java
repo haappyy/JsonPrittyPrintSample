20db8ae269dd7f742d0290f750ef8a6df193aaf2
Uniiquee <44533127+Uniiquee@users.noreply.github.com>
2021-10-29 23:45:38 +0200

    Add the json pretty print controller
    Adds a json pretty print controller which
    takes valid json input and pretty prints it.
    Fixes #8.

--- src/main/java/com/example/ExampleController.java
+++ src/main/java/com/example/ExampleController.java
@@ -1,18 +1,17 @@
 package com.example;
 
 import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 @Controller("/hello-world")
 public class ExampleController {
 
     private static final Logger LOG = LoggerFactory.getLogger(ExampleController.class);
 
-    @GET
+    @Get
     @Produces("text/plain")
     public String hello() {
         LOG.info("Request to hello");
