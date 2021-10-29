bb939d5c891f420e457f601d868928e552d1c733
Uniiquee <44533127+Uniiquee@users.noreply.github.com>
2021-10-29 22:51:57 +0200

    Add SL4J logging
    Add sl4j logging to the example controller. Fixes #4.

--- src/main/java/com/example/ExampleController.java
+++ src/main/java/com/example/ExampleController.java
@@ -1,6 +1,8 @@
 package com.example;
 
 import io.micronaut.http.annotation.Controller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -8,9 +10,12 @@ import javax.ws.rs.Produces;
 @Controller("/hello-world")
 public class ExampleController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(ExampleController.class);
+
     @GET
     @Produces("text/plain")
     public String hello() {
+        LOG.info("Request to hello");
         return "Hello, World!";
     }
 
